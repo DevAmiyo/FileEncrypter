acadaa46f12029a86a3f7417059980bcc9a38087
Amiyo <devamiyosp@gmail.com>
2022-06-01 08:59:54 +0530

    feat: ask key save location

--- decrypt.py
+++ decrypt.py
@@ -1,12 +1,12 @@
 from cryptography.fernet import Fernet
 from tkinter import filedialog
 
-keypath = filedialog.askopenfilename(title="Select a Key", filetypes=[("Key file", "*.key")])
+keypath = filedialog.askopenfilename(title="Select A Key", filetypes=[("Key File", "*.key")])
 
 with open(keypath, "rb") as secret:
     key = secret.read()
 
-filepath = filedialog.askopenfilename(title="Select a File", filetypes=[("All files", "*.*")])
+filepath = filedialog.askopenfilename(title="Select A File", filetypes=[("All Files", "*.*")])
 
 with open(filepath, "rb") as file:
     contents = file.read()
--- encrypt.py
+++ encrypt.py
@@ -1,12 +1,12 @@
 from cryptography.fernet import Fernet
 from tkinter import filedialog
 
-filepath = filedialog.askopenfilename(title="Select a File", filetypes=[("All files", "*.*")])
-
+keypath = filedialog.asksaveasfilename(title="Save Key As", filetypes=[("Key File", "*.key")])
 key = Fernet.generate_key()
-with open("secret.key", "wb") as secret:
+with open(keypath, "wb") as secret:
     secret.write(key)
 
+filepath = filedialog.askopenfilename(title="Select A File", filetypes=[("All Files", "*.*")])
 with open(filepath, "rb") as file:
     contents = file.read()
 encrypted = Fernet(key).encrypt(contents)
